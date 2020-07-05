c864f14e9b225920b602141c573f4a6ed35a9bb0
Ranjitha <ranjitha.r@c2info.com>
2020-07-05 21:25:59 +0530

    Updates

---
+++ precache-manifest.c88385e90c86e81f416ecc70172db3b4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4ba083d3d118c0ea0de70fed14e749",
+    "url": "/calendar-app/index.html"
+  },
+  {
+    "revision": "d893bc6ddee95b68c54f",
+    "url": "/calendar-app/static/css/2.b6ddee0d.chunk.css"
+  },
+  {
+    "revision": "cafc8d894aaae4a6416b",
+    "url": "/calendar-app/static/css/main.b8de4a11.chunk.css"
+  },
+  {
+    "revision": "d893bc6ddee95b68c54f",
+    "url": "/calendar-app/static/js/2.27574d2d.chunk.js"
+  },
+  {
+    "revision": "3dfeb62e21bcc1fd7d19c0bcd56acb2f",
+    "url": "/calendar-app/static/js/2.27574d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cafc8d894aaae4a6416b",
+    "url": "/calendar-app/static/js/main.98415897.chunk.js"
+  },
+  {
+    "revision": "d0df7be769ed098fdbe7",
+    "url": "/calendar-app/static/js/runtime-main.9794acae.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/calendar-app/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/calendar-app/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/calendar-app/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  }
+]);
